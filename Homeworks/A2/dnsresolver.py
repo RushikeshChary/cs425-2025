8520b1d0181aa7b24d1952bc444cec9d0f6c2d28
RushikeshChary <ruushid19@gmail.com>
2025-02-27 16:25:51 +0530

    Final comments/explanations along with some small changes are made

--- Homeworks/A2/dnsresolver.py
+++ Homeworks/A2/dnsresolver.py
@@ -24,9 +24,19 @@ def send_dns_query(server, domain):
         query = dns.message.make_query(domain, dns.rdatatype.A)  # Construct the DNS query
         # TODO: Send the query using UDP 
         # Note that above TODO can be just a return statement with the UDP query!
-
-        # Enable EDNS with a 4096-byte UDP buffer (increases response capacity)
+        """
+        EXPLANATION:
+        Enable EDNS with a 4096-byte UDP buffer (increases response capacity)
+        This increases the response capacity and may improve performance when resolving large domains
+        And, it will contain the ip addresses(in response.additional field) of all the domains provided in response.authority field.
+        If this line is not included, all the ip addresses of NS name severs are not recieved in the response.additional field.
+        """
         query.use_edns(0, payload=4096)
+        """
+        EXPLANATION:
+        Then, this function will make a query to given server(IP address) with query msg within the timout window.
+        response will be none if some error occured (like timeout).
+        """
         response = dns.query.udp(query, server, timeout=TIMEOUT)
         return response  # Return the response if successful
     except Exception:
@@ -48,10 +58,15 @@ def extract_next_nameservers(response):
                 ns_name = rr.to_text()
                 ns_names.append(ns_name)  # Extract nameserver hostname
                 print(f"Extracted NS hostname: {ns_name}")
-
     # TODO: Resolve the extracted NS hostnames to IP addresses
     # To TODO, you would have to write a similar loop as above
-    # Match extracted NS hostnames with IP addresses from the additional section
+    """
+    EXPLANATION:
+    Match extracted NS hostnames with IP addresses from the additional section
+    We can extract the IP addresses of all the name servers provided in authority field from the additional section of the response.
+    We have increased the UDP msg size to 4096 bytes from 512 bytes (refer to send dns_query function),
+    since all the IP addresses were not sent in additional field if default size is used.
+    """
     for rrset in response.additional:
         if rrset.rdtype == dns.rdatatype.A:
             for rr in rrset:
@@ -59,6 +74,10 @@ def extract_next_nameservers(response):
                     ns_ips.append(rr.address)
                     print(f"Resolved {rrset.name.to_text()} to {rr.address}")
 
+    """Used for debugging"""
+    # print(f"Number of NS records: {ns_names.__len__()}")
+    # print(f"Number of NS records resolved: {ns_ips.__len__()}")
+    
     return ns_ips  # Return list of resolved nameserver IPs
 
 def iterative_dns_lookup(domain):
@@ -73,7 +92,7 @@ def iterative_dns_lookup(domain):
     stage = "ROOT"  # Track resolution stage (ROOT, TLD, AUTH)
 
     while next_ns_list:
-        ns_ip = next_ns_list[0]  # Pick the first available nameserver to query
+        ns_ip = next_ns_list.pop(0)  # Pick the first available nameserver to query
         response = send_dns_query(ns_ip, domain)
         
         if response: #checks if response is not NONE
@@ -87,16 +106,22 @@ def iterative_dns_lookup(domain):
             # If no answer, extract the next set of nameservers
             next_ns_list = extract_next_nameservers(response)
             # TODO: Move to the next resolution stage, i.e., it is either TLD, ROOT, or AUTH
+            """
+            EXPLANATION:
+            After successfully resolving the nameserver,
+            we need to move to the next stage.
+            If we are in the ROOT stage, we move to the TLD stage.
+            If we are in the TLD stage, we move to the AUTH stage.
+            This way, we continue to the next stage until we either find an answer or reach the end of the resolution chain.
+            """
             if stage == "ROOT":
                 stage = "TLD"  # Move to TLD stage after querying ROOT servers
             elif stage == "TLD":
                 stage = "AUTH"  # Move to AUTH stage after querying TLD servers
-            elif stage == "AUTH":
-                print(f"[ERROR] No more nameservers to query")
-                return  # Stop resolution if no more nameservers are available
+
         else:
             print(f"[ERROR] Query failed for {stage} {ns_ip}")
-            return  # Stop resolution if a query fails
+            # return  # Stop resolution if a query fails
     
     print("[ERROR] Resolution failed.")  # Final failure message if no nameservers respond
 
@@ -110,6 +135,11 @@ def recursive_dns_lookup(domain):
     try:
         # TODO: Perform recursive resolution using the system's DNS resolver
         # Notice that the next line is looping through, therefore you should have something like answer = ??
+        """
+        EXPLANATION:
+        Since the loop provided is using answer, we need to populate it with "NS" name server's names and ip addresses.
+        That can be achieved by using resolver function with "NS" as parameter with the same domain.
+        """
         answer = dns.resolver.resolve(domain, "NS")
         for rdata in answer:
             print(f"[SUCCESS] {domain} -> {rdata}")
